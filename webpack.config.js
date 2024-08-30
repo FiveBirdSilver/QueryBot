f91cec2006bbc66080e800b2fab004dbe56f33f3
fivebirdsilver <tpdms0401@gmail.com>
2024-08-30 18:54:29 +0900

    [ci] 빌드 폴더명 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,27 @@
-const path = require("path");
-const CleanWebpackPlugin = require("clean-webpack-plugin").CleanWebpackPlugin;
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
-const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin').CleanWebpackPlugin
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 
 module.exports = {
   entry: {
-    popup: path.join(__dirname, "src/index.tsx"),
-    content: path.join(__dirname, "src/content.tsx"),
-    background: path.join(__dirname, "src/background.ts"),
+    popup: path.join(__dirname, 'src/index.tsx'),
+    content: path.join(__dirname, 'src/content.tsx'),
+    background: path.join(__dirname, 'src/background.ts'),
   },
   output: {
-    path: path.join(__dirname, "dist"),
-    filename: "[name].js",
+    path: path.join(__dirname, 'build'),
+    filename: '[name].js',
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      '@': path.resolve(__dirname, 'src'),
     },
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
     plugins: [
       new TsconfigPathsPlugin({
-        configFile: path.resolve(__dirname, "tsconfig.json"),
+        configFile: path.resolve(__dirname, 'tsconfig.json'),
       }),
     ],
   },
@@ -29,30 +29,30 @@ module.exports = {
     rules: [
       {
         test: /\.(ts|tsx)$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader", "postcss-loader"],
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
     ],
   },
-  mode: "production", // 혹은 'production'
+  mode: 'production', // 혹은 'production'
   plugins: [
     new CleanWebpackPlugin(),
     new CopyPlugin({
       patterns: [
         {
-          from: "public",
-          to: ".",
+          from: 'public',
+          to: '.',
         },
       ],
     }),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "public/index.html"),
-      filename: "popup.html",
-      chunks: ["popup"],
+      template: path.join(__dirname, 'public/index.html'),
+      filename: 'popup.html',
+      chunks: ['popup'],
     }),
   ],
-};
+}
