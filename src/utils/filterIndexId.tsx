446845964beca9706339f8e54e6c354020f4c721
fivebirdsilver <tpdms0401@gmail.com>
2024-09-14 17:07:38 +0900

    [perf] 불필요 파일 제거

--- src/utils/filterIndexId.tsx
+++
@@ -1,9 +0,0 @@
-const filterIndexId = (id: string) => {
-  if (id) {
-    const tmpSplit = id.split('_')
-    if (tmpSplit.length > 1) return id
-    else return `${id}_0`
-  }
-}
-
-export default filterIndexId
